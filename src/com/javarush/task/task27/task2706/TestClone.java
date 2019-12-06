353fb8d4197ff1e53091b43ccfd89762642863a5
George Tikhomirov <georgetikhomirov@mail.ru>
2019-12-06 19:25:06 +0300

    start

---
+++ src/com/javarush/task/task27/task2706/TestClone.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task27.task2706;
+
+public class TestClone  {
+    @Override
+    public TestClone clone() throws CloneNotSupportedException {
+        return this.clone();
+    }
+}
