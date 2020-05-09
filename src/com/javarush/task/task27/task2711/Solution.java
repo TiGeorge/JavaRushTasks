2d8af938d5c224bf595e665932420db9ad253529
TiGeorge <georgetikhomirov@mail.ru>
2020-05-09 23:20:29 +0300

    start

--- src/com/javarush/task/task27/task2711/Solution.java
+++ src/com/javarush/task/task27/task2711/Solution.java
@@ -6,25 +6,22 @@ import java.util.concurrent.CountDownLatch;
 CountDownLatch
 */
 public class Solution {
-    private final Object lock = new Object();
+    //private final Object lock = new Object();
     private volatile boolean isWaitingForValue = true;
 
     CountDownLatch latch = new CountDownLatch(1);
 
     public void someMethod() throws InterruptedException {
-        synchronized (lock) {
-            while (isWaitingForValue) {
-                lock.wait();
-            }
 
-            retrieveValue();
+        latch.await();
+        retrieveValue();
 
-            isWaitingForValue = false;
-            lock.notify();
-        }
+
+
     }
 
     void retrieveValue() {
+        latch.countDown();
         System.out.println("Value retrieved.");
     }
 
