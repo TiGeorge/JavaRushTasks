3064cb910ee19842b5ec33d07d61bd506bd84feb
TiGeorge <georgetikhomirov@mail.ru>
2020-05-11 21:09:51 +0300

    commit

--- src/com/javarush/task/task27/task2707/Solution.java
+++ src/com/javarush/task/task27/task2707/Solution.java
@@ -4,9 +4,10 @@ package com.javarush.task.task27.task2707;
 Определяем порядок захвата монитора
 */
 public class Solution {
+
     public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
-        synchronized (obj1) {
-            synchronized (obj2) {
+        synchronized (obj2) {
+            synchronized (obj1) {
                 System.out.println(obj1 + " " + obj2);
             }
         }
@@ -14,7 +15,25 @@ public class Solution {
 
     public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
         //do something here
-        return false;
+        Thread t1 = new Thread(() -> {
+            synchronized (o1) {
+                try {
+                    Thread.sleep(500);
+                    synchronized (o2) {
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            solution.someMethodWithSynchronizedBlocks(o1, o2);
+        });
+        t1.start();
+        t2.start();
+        Thread.sleep(2000);
+        return !(t2.getState() == Thread.State.BLOCKED);
+
     }
 
     public static void main(String[] args) throws Exception {
