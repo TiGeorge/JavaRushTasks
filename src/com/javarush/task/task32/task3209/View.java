59dfac70a410e5d535f5c72518c431f2e6916f90
TiGeorge <georgetikhomirov@mail.ru>
2020-05-17 22:08:39 +0300

    commit

--- src/com/javarush/task/task32/task3209/View.java
+++ src/com/javarush/task/task32/task3209/View.java
@@ -2,8 +2,10 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,10 +16,52 @@ public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
+    }
+
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
     }
 
     public void init() {
@@ -28,7 +72,17 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initMenuBar() {
+        JMenuBar jMenuBar = new JMenuBar();
 
+        MenuHelper.initFileMenu(this, jMenuBar); // file
+        MenuHelper.initEditMenu(this, jMenuBar); // edit
+        MenuHelper.initStyleMenu(this, jMenuBar); //style
+        MenuHelper.initAlignMenu(this, jMenuBar); // align
+        MenuHelper.initColorMenu(this, jMenuBar); //colorFont
+        MenuHelper.initFontMenu(this, jMenuBar); // fonts
+        MenuHelper.initHelpMenu(this, jMenuBar); //help
+
+        getContentPane().add(jMenuBar, BorderLayout.NORTH);
     }
 
     public void initEditor() {
